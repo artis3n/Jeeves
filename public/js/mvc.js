e03a1c526db30b2b381b56b8155751c75e01f43f
Ari <arikalfus@gmail.com>
2014-07-10 18:28:55 -0400

    Jeeves is broken. Implementing gapi.client.request broke the site. But I think I'm on the right track...

--- public/js/mvc.js
+++ public/js/mvc.js
@@ -21,4 +21,35 @@ jeevesApp.controller("jeevesCtrl", function($scope) {
 	$scope.changeView = function(selected) {
 		$scope.jeeves.view = selected;
 	};
+
+	$scope.listMessages = function(userId, query, callback) {
+  		var getPageOfMessages = function(request, result) {
+    		request.execute(function(resp) {
+      		result = result.concat(resp.messages);
+      		var nextPageToken = resp.nextPageToken;
+      		if (nextPageToken) {
+        		request = gapi.client.request(www.googleapis.com/gmail/v1/users/me/messages/list, function() {
+        			'userId': userId,
+        			'pageToken': nextPageToken,
+        			'q': query
+        		});
+        		getPageOfMessages(request, result);
+     			} else {
+     				callback(result);
+     			}
+    		});
+ 			};
+  		var initialRequest = gapi.client.request(www.googleapis.com/gmail/v1/users/me/messages/list, function() {
+        		'userId': userId,
+        		'q': query
+        		});
+  		var messages = getPageOfMessages(initialRequest, []);
+
+  		angular.forEach(messages, function(message) {
+  			var node = document.createElement("LI");
+  			var textnode = document.createTextNode(message);
+  			node.appendChild(textnode);
+  			document.getElementById("messageList").appendChild(node);
+  		})
+	}
 });
