c66758f923241ba93c45d9701faacb618234d3a2
wesleyyc <wesley.chin0919@gmail.com>
2014-07-11 13:35:31 -0400

    Change the Layout

--- public/js/mvc.js
+++ public/js/mvc.js
@@ -14,6 +14,7 @@ jeevesApp.run(function($http) {
 	$http.get("/model/feeds").success(function(data) {
 		model.feeds = data;
 	})
+	
 	// Waiting for the server to test the NYT API
 	// $http.get("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1?api-key=985d8028d34e7d34a098bfdd6b6b604c:5:67948289").success(function(data){
 	// 	console.log(data);
