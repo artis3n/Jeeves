fe9c7dce5185a5d7e75ff1a612601e8b754cc25e
Ari <arikalfus@gmail.com>
2014-07-14 11:42:54 -0400

    remove listmessages function

--- jeeves-demo/www/js/mvc.js
+++ jeeves-demo/www/js/mvc.js
@@ -33,38 +33,6 @@ jeevesApp.controller("jeevesCtrl", function($scope) {
 		}
 	};
 
-	$scope.listMessages = function(userId, query, callback) {
-  		
-  		var getPageOfMessages = function(request, result) {
-    		request.execute(function(resp) {
-      		result = result.concat(resp.messages);
-      		var nextPageToken = resp.nextPageToken;
-      		if (nextPageToken) {
-        		request = gmail.users().messages().list({
-          		'userId': userId,
-          		'pageToken': nextPageToken,
-          		'q': query
-        		}).execute();
-        		getPageOfMessages(request, result);
-     			} else {
-     					callback(result);
-     			}
-    		});
- 			};
-  		var initialRequest = gmail.users().messages().list({
-    		'userId': userId,
-    		'q': query
- 	 		}).execute();
-  		var messages = getPageOfMessages(initialRequest, []);
-
-  		angular.forEach(messages, function(message) {
-  			var node = document.createElement("LI");
-  			var textnode = document.createTextNode(message);
-  			node.appendChild(textnode);
-  			document.getElementById("messageList").appendChild(node);
-  		})
-  	}
-
 	// Changes weather widget to reflect new zip code as enterred by user.
 	// Precondition: zip code is 5 characters long. If not, throws alert error.
 	$scope.changeWeather = function() {
