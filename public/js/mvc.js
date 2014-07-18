41832c99678167a0dd852eb84cdd0f8235838812
Your Name <Sam@Sams-MacBook-Pro-2.local>
2014-07-18 10:56:01 -0400

    added script to mvc

--- public/js/mvc.js
+++ public/js/mvc.js
@@ -165,4 +165,22 @@ jeevesApp.controller("jeevesCtrl", function($scope, $http) {
 	$scope.collapse=function(){
 		$scope.jeeves.newsViews='';
 	}
+
+
+	$scope.reco= function(){
+		navigator.speechrecognizer.recognize(successCallback, failCallback, 1, "Cordova Speech Recognizer Plugin");
+
+		function successCallback(results){
+    		alert("Results: " + results);
+    		//if (results.match("/help/") === help){
+    			//$scope.changeView('help');
+    		//}
+ 		}
+
+		function failCallback(error){
+		    console.log("Error: " + error);
+
+		}
+		//return results;
+	}
 });
