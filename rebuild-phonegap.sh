9d29526cddc40e81e1445755f61d33839541787e
sam <samraphael@gmail.com>
2014-07-24 21:13:53 -0400

    added plugin/rebuilder for phonegap

---
+++ rebuild-phonegap.sh
@@ -0,0 +1,6 @@
+cordova platform add android
+cordova plugin add https://github.com/domaemon/org.apache.cordova.plugin.tts.git
+cordova plugin add https://github.com/manueldeveloper/cordova-plugin-speech-recognizer.git
+cordova plugin add https://github.com/oauth-io/oauth-phonegap
+cordova plugin add org.apache.cordova.dialogs
+phonegap build android
